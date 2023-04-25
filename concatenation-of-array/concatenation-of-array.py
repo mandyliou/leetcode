a63fec7c29cf69210d8cb7d5981b982dd28d9df8
mandyliou <111018795+mandyliou@users.noreply.github.com>
2023-04-24 20:10:27 -0700

    Time: 89 ms (44.00%) | Memory: 14.1 MB (52.83%) - LeetSync

--- concatenation-of-array/concatenation-of-array.py
+++ concatenation-of-array/concatenation-of-array.py
@@ -1,6 +1,3 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        new = nums.copy()
-        if len(nums) > 0:
-            nums += new
-        return nums
+        return nums * 2
