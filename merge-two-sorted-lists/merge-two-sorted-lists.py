458ea462d2a28c964cea73f34c7e18d5328f9876
mandyliou <111018795+mandyliou@users.noreply.github.com>
2023-04-25 18:52:47 -0700

    Time: 38 ms (64.00%) | Memory: 13.9 MB (20.25%) - LeetSync

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,17 +5,23 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
+        dummy = ListNode()
+        tail = dummy
+        #iterate thru linked list, compare values, and update pointers
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else: #list2 is less
+                tail.next = list2
+                list2 = list2.next 
+            tail = tail.next
         
-        # Recursive case
-        if list1.val <= list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
-
+        # If one of the lists still has nodes, add them to the merged list
+        if list1:
+            tail.next = list1
+        elif list2: #only 1 can be not null
+            tail.next = list2
+        #return head of merged list 
+        return dummy.next
 
