1158eb3b7725ef1b61b45a5f626e8a407746fd8f
mandyliou <111018795+mandyliou@users.noreply.github.com>
2023-04-26 21:42:56 -0700

    Time: 45 ms (7.15%) | Memory: 16.2 MB (5.56%) - LeetSync

--- middle-of-the-linked-list/middle-of-the-linked-list.py
+++ middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -7,12 +7,9 @@
 
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
-        new = []
-        current = head
-
-        while current:
-            new.append(current)
-            current = current.next
-        mid_index = len(new) // 2
-        
-        return new[mid_index]
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next #2 steps ahead of slow
+        return slow
