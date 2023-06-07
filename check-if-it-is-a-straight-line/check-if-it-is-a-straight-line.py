c27efec85b678007ed605387ead48c10d911ddf5
mandyliou <111018795+mandyliou@users.noreply.github.com>
2023-06-07 10:22:24 -0700

    Time: 71 ms (84.68%) | Memory: 16.9 MB (22.44%) - LeetSync

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
@@ -3,14 +3,15 @@ class Solution:
         #slope = y2-y1 / x2-x1 = [1][1] - [0][1] / [1][0] - [0][0]
         # m = (coor[1][1] - coor[0][1]) / (coor[1][0] - coor[0][0])
         #this method will check for same ratio aka same slope
-        dy = coor[1][1] - coor[0][1] #dy= 3-2=1
-        dx= coor[1][0] - coor[0][0] #dx=2-1=1
+        x0, y0 = coor[0]
+        x1, y1 = coor[1]
+
         for i in range(2, len(coor)):
+            x, y = coor[i]
             #if ratio of diff is NOT constant, return False
-            if (dy * (coor[i][0] - coor[0][0])) != (dx * (coor[i][1] - coor[0][1])):
+            if ((x - x0) * (y1 - y0)) != ((y - y0) * (x1 - x0)):
                 return False
         #all counter-example fall within the slope
         return True
 
-#1* (3-1)= 2
-#1 * (4-2) = 2
+
