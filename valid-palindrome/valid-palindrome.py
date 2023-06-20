b4ef67ffdc4acad1f5df89ec3c0d6666967b97ac
mandyliou <111018795+mandyliou@users.noreply.github.com>
2023-06-20 12:08:18 -0700

    Time: 54 ms (85.85%) | Memory: 17.8 MB (26.15%) - LeetSync

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,10 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new = ""
-        for i in s:
-            if i.isalnum(): #removes punctuations, whitespaces; considers digits 
-                new += i.lower() #changes to lower case
-        return new == new[::-1] #returns true if palindrome
+        s = [i for i in s.lower() if i.isalnum()]
+        return s == s[::-1]
 
         
         
