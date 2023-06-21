ce03698b2eaf3c36f6cdbad4fb27447c94ff4331
mandyliou <111018795+mandyliou@users.noreply.github.com>
2023-06-21 16:06:36 -0700

    Time: 51 ms (92.61%) | Memory: 17.8 MB (26.41%) - LeetSync

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,7 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = [i for i in s.lower() if i.isalnum()]
-        return s == s[::-1]
+        new = []
+        for i in s.lower(): #convert to lowercase
+            if i.isalnum(): #check alphanumeric by .isalnum()
+                new.append(i)
+                
+        return new == new[::-1] #return true if new is same reversed
 
         
         
