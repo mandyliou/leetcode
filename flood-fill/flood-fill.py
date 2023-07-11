add583c9bf85c5e8d33a76ead7b48741a1652790
mandyliou <111018795+mandyliou@users.noreply.github.com>
2023-07-10 21:19:53 -0700

    Time: 95 ms (30.73%) | Memory: 16.7 MB (23.07%) - LeetSync

--- flood-fill/flood-fill.py
+++ flood-fill/flood-fill.py
@@ -1,19 +1,34 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # Store the original color of the starting pixel
         oldColor = image[sr][sc]
+        
+        # If the original color is the same as the new color, no changes are needed
+        # so we return the original image
         if oldColor == color:
             return image
+
+        # Get the dimensions of the image
         rows = len(image)
         cols = len(image[0])
+
         def dfs(r, c):
-        #if current pixel's out of bounds, or current cell is same color -> return
+            # If the current pixel is out of the image boundaries or the color 
+            # of the current pixel is not the same as the original color, return
             if (r < 0 or c < 0) or (r >= rows or c >= cols) or (image[r][c] != oldColor):
                 return
-            #mark current cell as 0 if it's been visited
-            image[r][c] = color #color current pixel with new color
-            dfs(r + 1, c) #down
-            dfs(r - 1, c) #up
-            dfs(r, c + 1) #right
-            dfs(r, c - 1) #left
+
+            # Change the color of the current pixel to the new color
+            image[r][c] = color
+
+            # Perform a depth-first search on the 4 neighbouring pixels
+            dfs(r + 1, c) # Move down
+            dfs(r - 1, c) # Move up
+            dfs(r, c + 1) # Move right
+            dfs(r, c - 1) # Move left
+
+        # Start the depth-first search from the starting pixel
         dfs(sr, sc)
+        
+        # Return the modified image after performing the flood fill
         return image
