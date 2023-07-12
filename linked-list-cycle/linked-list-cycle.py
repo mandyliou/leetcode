b036e504f629c26d212af164e0ec710993a6c77e
mandyliou <111018795+mandyliou@users.noreply.github.com>
2023-07-11 20:59:43 -0700

    Time: 75 ms (54.52%) | Memory: 20.3 MB (85.90%) - LeetSync

--- linked-list-cycle/linked-list-cycle.py
+++ linked-list-cycle/linked-list-cycle.py
@@ -6,16 +6,12 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if head is None: #if list is empty or only has 1 node, can't have cycle
-            return False
-        slow = head #starts at 1st node
-        fast = head.next #starts at 2nd node
-
-        while slow != fast: #while slow and fast do NOT meet...
-            #if fast pointer reached end of list, there's no cycle
-            if fast is None or fast.next is None: 
-                return False
+        slow = head
+        fast = head
+        while fast and fast.next: 
             slow = slow.next #moves slow pointer 1 step forward
             fast = fast.next.next #moves fast pointer 2 steps forward
-        return True #if slow and fast meet, then there's a cycle
+            if slow == fast:#if slow and fast meet, then there's a cycle
+                return True
+        return False #slow and fast never meet, no cycle 
 
